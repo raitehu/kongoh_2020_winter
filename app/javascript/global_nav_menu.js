187482cb3535f807eb0f4b63d8aef0ce2ea4a566
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2020-09-12 14:32:58 +0900

    トップページ完成

---
+++ app/javascript/global_nav_menu.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('#global_nav_menu_open').on('click', function(){
+        $('.global_nav').fadeToggle();
+    });
+    $('#global_nav_menu_close').on('click', function(){
+        $('.global_nav').fadeToggle();
+    });
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("global_nav_menu")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
