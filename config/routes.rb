18d27022ec3436918fef245e7866225b977a36ea
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2020-09-15 01:11:24 +0900

    ロジック部分は完成
    TODO: CSS

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'welcome_event/index'
   get 'recital_plan/index'
   get '/', to: 'top#index', as: 'root'
+  get 'about', to: 'about_kongoh#index', as: 'about'
   get 'recital_plan', to: 'recital_plan#index', as: 'recital_plan'
 
   get 'welcome_event', to: 'welcome_event#index', as: 'welcome_event'
