5540e8fe98f034cf44237aa694c6e6b00a61c678
Raitehu KIRISHIMA <pleiades-union@dune.ocn.ne.jp>
2020-09-15 23:27:25 +0900

    公演記録ページ完成
    TODO: 写真系

--- app/models/recital.rb
+++ app/models/recital.rb
@@ -3,13 +3,19 @@ class Recital < ApplicationRecord
   has_many :programs, dependent: :destroy
   has_many :recital_photos, dependent: :destroy
 
+  extend DateTimeConcern
+
   scope :plans, -> { where('date >= ?', Date.today).order(date: :asc) }
   scope :pasts, -> { where('date < ?',  Date.today).order(date: :desc) }
+  scope :in_fiscal_year, ->(year) { where(date: fiscal_year_range(year)).order(date: :desc) }
+
+  scope :latest_year, -> { order(date: :desc).first.date.year }
+  scope :oldest_year, -> { order(date: :asc).first.date.year }
 
   scope :with_photos, -> { joins(:recital_photos) }
 
-  def date
-    super.strftime('%Y年%m月%e日')
+  def str_date
+    date.strftime('%Y年%m月%e日')
   end
 
   def opening_time
